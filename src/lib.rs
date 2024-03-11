43c7e250d185e70c322beda3fa8ab8556216737b
JktuJQ <odmamontov@gmail.com>
2024-03-11 19:15:45 +0300

    [Lints] Added lints at crate level
    Added several lints for concise code in 'src/lib.rs'.

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,9 @@
 //!
 //! **GGEngine** - 2d game engine written in pure Rust that implements Unity-like interface.
 //!
+
+#![warn(missing_docs, clippy::missing_docs_in_private_items)] // `missing_docs`
+#![warn(unused_import_braces, unused_qualifications, unused_results)] // `unused_*`
+#![warn(trivial_casts, trivial_numeric_casts)] // `casts`
+#![warn(missing_copy_implementations, missing_debug_implementations)] // `missing_*_implementations`
+#![warn(variant_size_differences, unreachable_pub)]
