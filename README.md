1be5e4cbc825b86547535b13744dfa3b9928a206
JktuJQ <odmamontov@gmail.com>
2024-03-11 19:10:55 +0300

    First commit
    Created basic project structure ('Cargo.toml', 'src/lib.rs' and licenses). This is the first commit to this repository! 🥳🥳🥳

---
+++ README.md
@@ -0,0 +1,3 @@
+# GGEngine
+
+**GGEngine** - 2d game engine written in pure Rust that implements Unity-like interface.
---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+//! # GGEngine
+//!
+//! **GGEngine** - 2d game engine written in pure Rust that implements Unity-like interface.
+//!
