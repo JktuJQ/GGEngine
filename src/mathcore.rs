e9b130c579b29d9c03145304d082033f575b787f
JktuJQ <odmamontov@gmail.com>
2024-03-16 19:13:11 +0300

    [Documentation] Completed `mathcore` module
    Completed `mathcore` module and added documentation to it.

--- src/mathcore.rs
+++ src/mathcore.rs
@@ -1,5 +1,17 @@
 //! `ggengine::mathcore` module is a core that implements all math functionality for engine.
 //!
+//! # Model
+//! There are several very important constructs that are essential to game engine.
+//! Vectors usually represent directions and coordinates.
+//! With that in mind, it's natural to implement transformations of objects as
+//! matrices.
+//! Shapes are represented by their vertices (vectors), so translation, rotation and other transformations are easy to apply.
+//! Shape collision is checked using geometry.
+//!
+//! That model is quite common for many game engines,
+//! so almost everyone with basic knowledge of game developing should be familiar with `ggengine`.
+//!
+
 // submodules and public re-exports
 mod ext;
 pub use ext::*;
