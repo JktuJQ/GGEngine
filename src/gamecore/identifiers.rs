e070252a2b7e4dac5c4b5792026505dc249f39fa
JktuJQ <odmamontov@gmail.com>
2024-07-23 22:47:26 +0300

    [Development] Added ECS indexes
    Added ECS indexes for `Component` and `GameObject`. Those will help identifying them in the `Scene`.

--- src/gamecore/identifiers.rs
+++ src/gamecore/identifiers.rs
@@ -1,3 +1,48 @@
-//! `gamecore::identifiers` submodule provides several `Id` structs that
+//! `gamecore::identifiers` submodule provides several id structs that
 //! uniquely identify objects and allows easy querying on them.
 //!
+
+/// [`impl_id`] macro implements basic id struct interface.
+///
+/// For most id structs, some kind of uniqueness is required for their sound usage.
+/// That uniqueness is not provided by this struct, and that is why `new` associated function is
+/// exposed only to crate visibility - it is caller's job to provide soundness.
+///
+macro_rules! impl_id {
+    ($struct:ident) => {
+        impl $struct {
+            /// Creates new id with given value.
+            ///
+            pub(crate) fn new(id: usize) -> Self {
+                Self(id)
+            }
+
+            /// Returns underlying id.
+            ///
+            pub fn value(&self) -> usize {
+                self.0
+            }
+        }
+    };
+}
+
+/// [`ComponentId`] id struct is needed to identify [`Component`](super::components::Component)s
+/// in [`Scene`](super::scenes::Scene).
+///
+/// It is assigned by the [`Scene`](super::scenes::Scene) in which
+/// [`GameObject`](super::gameobjects::GameObject) with
+/// this [`Component`](super::components::Component) is registered.
+///
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct ComponentId(usize);
+impl_id!(ComponentId);
+
+/// [`GameObjectId`] id struct is needed to identify [`GameObject`](super::gameobjects::GameObject)s
+/// in [`Scene`](super::scenes::Scene).
+///
+/// It is assigned by the [`Scene`](super::scenes::Scene) in which
+/// [`GameObject`](super::gameobjects::GameObject) is registered.
+///
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct GameObjectId(usize);
+impl_id!(GameObjectId);
