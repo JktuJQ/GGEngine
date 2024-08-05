d8059b9c67206f25db593832ba102b37e73fcbd1
JktuJQ <odmamontov@gmail.com>
2024-08-05 19:24:51 +0300

    [Development] Encapsulated `Component` trait upcasting
    Encapsulated `Component` trait upcasting to `&dyn Any` in a `as_any` hidden module.

--- src/gamecore/components.rs
+++ src/gamecore/components.rs
@@ -9,14 +9,34 @@ use std::{
     fmt,
 };
 
-pub trait Component: Any {
-    fn as_any(&self) -> &dyn Any;
-}
-impl<T: Any> Component for T {
-    fn as_any(&self) -> &dyn Any {
-        self
+/// `as_any` hidden module is needed to hide trait upcasting workaround.
+/// 
+/// Currently, trait upcasting is not stable,
+/// but it is needed to use runtime reflextion (`&dyn Component` to `&dyn Any`).
+/// Workaround is implemented in `AsAny` trait
+/// 
+pub(in crate::gamecore) mod as_any {
+    use std::any::Any;
+
+    /// [`AsAny`] trait is a workaround for trait upcasting.
+    /// 
+    /// [`AsAny`] trait blanked implementation for all types that implement `Any`
+    /// allows for `&self` to coerce to `&dyn Any`.
+    /// 
+    pub trait AsAny {
+        /// Method that coerces `&self` to `&dyn Any`.
+        /// 
+        fn as_any(&self) -> &dyn Any;
+    }
+    impl<T: Any> AsAny for T {
+        fn as_any(&self) -> &dyn Any {
+            self
+        }
     }
 }
+
+pub trait Component: Any + as_any::AsAny {}
+impl<T: Any> Component for T { }
 
 impl fmt::Debug for dyn Component {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
